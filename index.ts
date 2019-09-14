70f85d994829e20a9feccc681db5fbf70442f809
JCGrant <jamescolin.grant@gmail.com>
2019-09-14 04:15:10 +0100

    Update for new Barclays page layout
    Add a few more console logs

--- index.ts
+++ index.ts
@@ -12,9 +12,11 @@ async function getRequestedMemorableWordCharacters(
 }
 
 async function getBalance(page: puppeteer.Page): Promise<number> {
-  const element = await page.$('.balance.fixGoalSpan2');
+  await page.waitForSelector('.o-account__balance-head');
+  const element = await page.$('.o-account__balance-head');
   const text = await page.evaluate(element => element.textContent, element);
-  return parseFloat(text.slice(1).replace(',', ''));
+  const balanceStr = text.trim().replace('£', '').replace(',', '');
+  return parseFloat(balanceStr);
 }
 
 interface LoginDetails {
@@ -30,8 +32,10 @@ async function login(
   { surname, sortCode, accountNumber, passcode, memorableWord }: LoginDetails
 ) {
   console.log('identifying...');
+  console.log('typing surname...');
   await page.type('#surname0', surname);
   await page.click('#label-sortCode-main');
+  console.log('typing sort code...');
   await page.type('#sortCode0', sortCode.slice(0, 2));
   await page.type('#sortCode1', sortCode.slice(2, 4));
   await page.type('#sortCode2', sortCode.slice(4, 6));
@@ -42,11 +46,13 @@ async function login(
   ]);
 
   console.log('authenticating...');
+  console.log('typing passcode...');
   await page.type('#passcode0', passcode);
   const [c1, c2] = await getRequestedMemorableWordCharacters(
     page,
     memorableWord
   );
+  console.log('entering memorable word characters...');
   const radioDropdowns = await page.$$('#selectedCharacter');
   await radioDropdowns[0].click();
   await page.keyboard.type(c1);
@@ -71,10 +77,13 @@ async function main() {
   });
   const page = await browser.newPage();
   await page.goto(BARCLAYS_LOGIN_URL, { waitUntil: 'networkidle2' });
+  console.log('logging in...');
   await login(page, require(BANK_LOGIN_PATH));
+  console.log('getting balance...');
   const balance = await getBalance(page);
   console.log('balance:', balance);
   await browser.close();
+  console.log('updating sheet...');
   updateSheet(balance);
 }
 
